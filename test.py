7cd04aba55f70a5255cb2b11f7f0210ee561155b
Guillaume Anciaux <guillaume.anciaux@epfl.ch>
2023-01-31 09:26:01 +0100

    adding stopwords

--- test.py
+++ test.py
@@ -6,10 +6,260 @@ import matplotlib.pyplot as plt
 st.title("EPFL Teaching Day 2023: word cloud")
 
 
+additional_stopwords = """
+tout
+grand
+petit
+même
+autre
+seul
+jeune
+premier
+bon
+quel
+beau
+vieux
+noir
+nouveau
+dernier
+blanc
+cher
+long
+pauvre
+plein
+toute
+bas
+gros
+doux
+heureux
+haut
+profond
+rouge
+humain
+général
+froid
+sombre
+sûr
+ancien
+propre
+politique
+possible
+immense
+public
+pareil
+bleu
+fort
+entier
+simple
+nécessaire
+mauvais
+social
+important
+certain
+triste
+joli
+différent
+ne
+pas
+plus
+bien
+si
+là
+encore
+aussi
+peu
+alors
+toujours
+jamais
+non
+très
+ainsi
+moins
+ici
+oui
+trop
+déjà
+tant
+enfin
+maintenant
+beaucoup
+assez
+loin
+point
+presque
+ailleurs
+seulement
+aujourd'hui
+mieux
+autour
+souvent
+dessus
+longtemps
+comme
+comment
+autant
+d'abord
+surtout
+cependant
+vite
+tard
+pourtant
+ci
+mal
+parfois
+vraiment
+bientôt
+partout
+debout
+plutôt
+lentement
+combien
+hier
+et
+que
+mais
+ou
+quand
+puis
+donc
+car
+ni
+parce que
+pourquoi
+lorsque
+tandis que
+puisque
+soit
+or
+le
+un
+son
+ce
+du
+au
+de
+mon
+leur
+notre
+votre
+quelque
+ton
+chaque
+aucun
+tel
+plusieurs
+d'autres
+nul
+deux
+cent
+mille
+trois
+quatre
+vingt
+cinq
+dix
+neuf
+six
+huit
+sept
+trente
+quarante
+cinquante
+quinze
+douze
+à
+en
+dans
+pour
+par
+sur
+avec
+sans
+sous
+après
+entre
+vers
+chez
+jusque
+contre
+devant
+depuis
+pendant
+avant
+voilà
+près
+dès
+malgré
+voici
+selon
+derrière
+parmi
+afin de
+auprès
+quant à
+hors
+durant
+grâce
+il
+je
+se
+qui
+elle
+vous
+me
+on
+lui
+nous
+y
+où
+tu
+moi
+te
+celui
+rien
+dont
+ça
+cela
+toi
+lequel
+quoi
+personne
+l'un
+chacun
+auquel
+quelqu'un
+ceci
+l'une
+soi
+sien
+mien
+la
+les
+des
+une
+quels
+quelle
+quelles
+lesquels
+laquelle
+lesquelles
+duquel
+afin
+aux
+""".split()
+
+
+comment_words = ''
+stopwords = list(STOPWORDS)
+stopwords += additional_stopwords
+stopwords = set(stopwords)
+stopwords = '\n'.join(stopwords)
+
+st.text_area("Words to ignore", value=stopwords)
 _file = st.file_uploader('Upload your text file here')
+
+
 if _file is not None:
-    comment_words = ''
-    stopwords = set(STOPWORDS)
     text = _file.read().decode()
     text = text.replace('\n', ' ')
     text = text.split()
@@ -19,7 +269,7 @@ if _file is not None:
 
     wordcloud = WordCloud(width=1500, height=int(1500*9/16),
                           background_color='white',
-                          stopwords=stopwords,
+                          stopwords=stopwords.split(),
                           min_font_size=10).generate(comment_words)
 
     fig = plt.figure(figsize=(8, 8), facecolor=None)
